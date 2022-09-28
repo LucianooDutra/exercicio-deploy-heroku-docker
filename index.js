49298a42f36aeb2938a856a2b3af973533bad44a
Luciano Dutra Cavalcante de Souza <lucianoo_dutra@hotmail.com>
2022-09-28 19:26:45 -0300

    feat: criação do heroku com docker

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3002;
+
+app.get("/", (_req, res) => {
+  res.send(`<h1> Executando na porta: ${PORT} </h1>`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Rodando na porta ${PORT}`);
+});
